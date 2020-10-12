553c49177f9214ce773c022104494c13f2941045
oguzhangungor <oguzhan.gngr91@gmail.com>
2020-10-12 18:23:01 +0300

    Marking places around the current location and requesting location permission from the user

--- app/src/main/java/com/ogungor/kotlinnearmapplace/MapsActivity.kt
+++ app/src/main/java/com/ogungor/kotlinnearmapplace/MapsActivity.kt
@@ -1,12 +1,14 @@
 package com.ogungor.kotlinnearmapplace
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
+import android.util.Log
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -20,6 +22,14 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.ogungor.kotlinnearmapplace.Common.Common
+import com.ogungor.kotlinnearmapplace.Model.MyPlaces
+import com.ogungor.kotlinnearmapplace.Remote.IGoogleAPIService
+import kotlinx.android.synthetic.main.activity_maps.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.lang.StringBuilder
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -35,7 +45,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     //Location
 
-    lateinit var fusedLocationLocationClient: FusedLocationProviderClient
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     lateinit var locationRequest: LocationRequest
     lateinit var locationCallback: LocationCallback
 
@@ -43,6 +53,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     companion object {
         private val MY_PERMISSION_CODE = 1
     }
+
+    lateinit var mService: IGoogleAPIService
+
+    internal lateinit var currentPlace: MyPlaces
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,6 +67,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+        //Inıt Service
+
+        mService = Common.googleAPIService
+
 
         //Request runtime Permission
 
@@ -61,8 +79,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 buildLocationRequest()
                 buildLocationCallBack()
 
-                fusedLocationLocationClient = LocationServices.getFusedLocationProviderClient(this)
-                fusedLocationLocationClient.requestLocationUpdates(
+                fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+                fusedLocationProviderClient.requestLocationUpdates(
                     locationRequest,
                     locationCallback,
                     Looper.myLooper()
@@ -72,21 +90,148 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             buildLocationRequest()
             buildLocationCallBack()
 
-            fusedLocationLocationClient = LocationServices.getFusedLocationProviderClient(this)
-            fusedLocationLocationClient.requestLocationUpdates(
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+            fusedLocationProviderClient.requestLocationUpdates(
                 locationRequest, locationCallback,
                 Looper.myLooper()
             )
         }
 
+        bottom_navigator_view.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.action_hospital -> nearByPlace("hospital")
+                R.id.action_market -> nearByPlace("market")
+                R.id.action_restaurant -> nearByPlace("restaurant")
+                R.id.school -> nearByPlace("school")
+            }
+            true
+        }
 
+
+    }
+
+    private fun nearByPlace(typePlace: String) {
+
+        //Clear All Marker on Map
+        mMap.clear()
+
+        //Build URL request base on location
+
+        val url = getUrl(latitude, longitude, typePlace)
+
+        mService.getNearbyPlaces(url)
+            .enqueue(object : Callback<MyPlaces> {
+                override fun onResponse(call: Call<MyPlaces>, response: Response<MyPlaces>) {
+                    currentPlace = response.body()!!
+
+                    if (response!!.isSuccessful) {
+
+                        for (i in 0 until response!!.body()!!.results!!.size) {
+                            val markerOptios = MarkerOptions()
+                            val googlePlace = response.body()!!.results!![i]
+                            val lat = googlePlace.geometry!!.location!!.lat
+                            val lng = googlePlace.geometry!!.location!!.lng
+                            val placeName = googlePlace.name
+                            val latLng = LatLng(lat, lng)
+
+
+                            markerOptios.position(latLng)
+                            markerOptios.title(placeName)
+
+                            if (typePlace.equals("hospital")) {
+                                markerOptios.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_hospital))
+                            } else if (typePlace.equals("market")) {
+                                markerOptios.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_shopping))
+                            } else if (typePlace.equals("restaurant")) {
+                                markerOptios.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_restaurant))
+                            } else if (typePlace.equals("school")) {
+                                markerOptios.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_school))
+                            } else {
+                                markerOptios.icon(
+                                    BitmapDescriptorFactory.defaultMarker(
+                                        BitmapDescriptorFactory.HUE_RED
+                                    )
+                                )
+
+                            }
+                           /* if (typePlace.equals("hospital")) {
+                                markerOptios.icon(
+                                    BitmapDescriptorFactory.defaultMarker(
+                                        BitmapDescriptorFactory.HUE_RED
+                                    )
+                                )
+                            } else if (typePlace.equals("market")) {
+                                markerOptios.icon(
+                                    BitmapDescriptorFactory.defaultMarker(
+                                        BitmapDescriptorFactory.HUE_RED
+                                    )
+                                )
+                            } else if (typePlace.equals("restaurant")) {
+                                markerOptios.icon(
+                                    BitmapDescriptorFactory.defaultMarker(
+                                        BitmapDescriptorFactory.HUE_RED
+                                    )
+                                )
+                            } else if (typePlace.equals("school")) {
+                                markerOptios.icon(
+                                    BitmapDescriptorFactory.defaultMarker(
+                                        BitmapDescriptorFactory.HUE_RED
+                                    )
+                                )
+                            } else {
+                                markerOptios.icon(
+                                    BitmapDescriptorFactory.defaultMarker(
+                                        BitmapDescriptorFactory.HUE_RED
+                                    )
+                                )
+
+                            }*/
+                            markerOptios.snippet(typePlace.toString())  //Alt Bilgi
+
+                            //Add marker to map
+
+                            mMap.addMarker(markerOptios)
+
+
+                        }
+                        // Move Camera Zoom
+
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(latitude,longitude), 16f))
+                        mMap.animateCamera(CameraUpdateFactory.zoomTo(17f))
+
+
+                    }
+                }
+
+                override fun onFailure(call: Call<MyPlaces>, t: Throwable) {
+                    (Toast.makeText(baseContext, "..." + t.message, Toast.LENGTH_LONG).show())
+                }
+
+
+            })
+
+
+    }
+
+    private fun getUrl(latitude: Double, longitude: Double, typePlace: String): String {
+
+        val googlePlaceUrl =
+            StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json")
+        googlePlaceUrl.append("?location=$latitude,$longitude")
+        googlePlaceUrl.append("&radius=300") // 10km aralığında
+        googlePlaceUrl.append("&type=$typePlace")
+        googlePlaceUrl.append("&key=AIzaSyBLWNF4ChIdv8co_K8vtXsd3d3_z6DDrLc")
+
+        Log.d("URL_DEBUG", googlePlaceUrl.toString())
+        return googlePlaceUrl.toString()
     }
 
     private fun buildLocationCallBack() {
 
+        // Son lokasyonu alıyoruz
         locationCallback = object : LocationCallback() {
-            override fun onLocationResult(loc: LocationResult?) {
-                mLastLocation = loc!!.locations.get(loc!!.locations.size - 1) //Get Last Location
+            override fun onLocationResult(location: LocationResult?) {
+                mLastLocation = location!!.locations.get(location!!.locations.size - 1) //Get Last Location
                 if (mMarker != null) {
                     mMarker!!.remove()
                 }
@@ -102,9 +247,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 mMarker = mMap!!.addMarker(markerOptions)
 
 
-                //Move Camera
+                //Move Camera zoom
 
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLang, 11f))
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLang, 17f))
 
                 // https://www.youtube.com/watch?v=SHn-K37zOZU&ab_channel=EDMTDev
 
@@ -114,6 +259,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     }
 
+
+    @SuppressLint("RestrictedApi")
     private fun buildLocationRequest() {
 
         locationRequest = LocationRequest()
@@ -123,7 +270,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         locationRequest.smallestDisplacement = 10f
     }
 
+
+
+    //Kullanıcının iznini kontrol ediyoruz bunu splash ekranında yapacağız.
     private fun checkLocationPermission(): Boolean {
+
+
 
         if (ContextCompat.checkSelfPermission(
                 this,
@@ -142,11 +294,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                     MY_PERMISSION_CODE
                 )
             else
-                ActivityCompat.requestPermissions(
+            ActivityCompat.requestPermissions(
                     this,
                     arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
                     MY_PERMISSION_CODE
                 )
+
 
             return false
         } else
@@ -155,9 +308,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
 
-//Override OnRequestPermissionResult
+//Override OnRequestPermissionResult /Kullanıcı izin vermiş mi vermemiş mi
 
     override fun onRequestPermissionsResult(
+
+
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
@@ -176,9 +331,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                             buildLocationCallBack()
 
 
-                            fusedLocationLocationClient =
+                            fusedLocationProviderClient =
                                 LocationServices.getFusedLocationProviderClient(this)
-                            fusedLocationLocationClient.requestLocationUpdates(
+                            fusedLocationProviderClient.requestLocationUpdates(
                                 locationRequest, locationCallback,
                                 Looper.myLooper()
                             )
@@ -194,7 +349,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     override fun onStop() {
-        fusedLocationLocationClient.removeLocationUpdates(locationCallback)
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
         super.onStop()
     }
 
